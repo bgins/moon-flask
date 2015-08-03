dfcc43622ea51eb3b3020e2f29af7537a6913364
Brian Ginsburg <bginsburg3@gmail.com>
2015-08-03 11:24:41 -0700

    Nested test dictionary with more attributes. Edited index.html to display entry and its about

--- app/views.py
+++ app/views.py
@@ -1,9 +1,14 @@
 from app import app
 from flask import render_template, url_for
 
-interests = {'code':'This is the code page', 
-			 'writing':'A page on writing',
-			 'music':'A page on music'}
+# interests = {'code': 'This is the code page', 
+# 			 'text':'This is the text page',
+# 			 'music': 'This is the music page'}
+
+interests = {'code': {'about': 'This is the code page', 'image':'/static/code.png', 'color': 'red'}, 
+			 'text': {'about': 'This is the text page', 'image':'/static/text.png', 'color': 'blue'},
+			 'music':{'about': 'This is the music page', 'image':'/static/music.png', 'color': 'green'}
+		}
 
 
 @app.route('/')
