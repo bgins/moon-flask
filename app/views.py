48cf7b2a1b90ff93cd2e603f0a31d7e33ae438e1
Brian Ginsburg <bginsburg3@gmail.com>
2015-08-07 14:45:01 -0700

    Added profile photo for user

--- app/views.py
+++ app/views.py
@@ -7,7 +7,7 @@ interests = {1 : {'area':'code', 'about': 'This is the code page', 'color': 'red
 			 3 : {'area':'music', 'about': 'This is the music page', 'color': 'green', 'FKUserId': 1}
 		}
 
-users = {1: {'name': 'admin',' password': 'default'}}
+user = {1: {'name': 'admin','password': 'default'}}
 
 posts = {1: {'title': 'Python', 'text': 'I like Python', 'FK_IntrestId': 1},
 		 2: {'title': 'Flask', 'text': 'This is built on Flask', 'FK_IntrestId': 1},
@@ -18,7 +18,7 @@ posts = {1: {'title': 'Python', 'text': 'I like Python', 'FK_IntrestId': 1},
 
 @app.route('/')
 def index():
-	return render_template('index.html', interests=interests)
+	return render_template('index.html', interests=interests, user=user)
 
 @app.route('/interest/<area>')
 def interest(area):
