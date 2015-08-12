11c793f14112ed4b3b8781bb977f5598e1dbe942
Brian Ginsburg <bginsburg3@gmail.com>
2015-08-12 09:51:32 -0700

    Ditched tenuous navbar hack for flexbox layout.

--- app/views.py
+++ app/views.py
@@ -3,10 +3,7 @@ from flask import render_template, url_for
 
 interests = {1 : {'area':'code', 'about': 'This is the code page', 'color': '#936FB2', 'FKUserId': 1}, 
 			 2 : {'area':'text', 'about': 'This is the text page', 'color': '#AF4415', 'FKUserId': 1},
-			 3 : {'area':'music', 'about': 'This is the music page', 'color': '#5487B2', 'FKUserId': 1},
-			 4 : {'area':'music', 'about': 'This is the music page', 'color': '#5487B2', 'FKUserId': 1},
-
-
+			 3 : {'area':'music', 'about': 'This is the music page', 'color': '#5487B2', 'FKUserId': 1}
 		}
 
 user = {1: {'name': 'Gentile Bellini','password': 'default'}}
@@ -20,18 +17,8 @@ posts = {1: {'title': 'Python', 'text': 'I like Python', 'FK_IntrestId': 1},
 
 @app.route('/')
 def index():
-	link_width = calculate_width();
-	return render_template('index.html', interests=interests, user=user, link_width=link_width)
+	return render_template('index.html', interests=interests, user=user)
 
 @app.route('/interest/<area>')
 def interest(area):
 	return render_template('interest.html', interests=interests)
-
-def calculate_width():
-	# Convert number to word for skeleton.css class - five links won't center properly!
-	numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve"]
-	offset = None
-	if len(interests) == 5:
-		offset = True
-	link_width = int(12 / len(interests))
-	return {'width':numbers[link_width], 'offset':offset}
