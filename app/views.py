d45a8bf5d80bee67a8fd4bb6156aae000948c717
Brian Ginsburg <bginsburg3@gmail.com>
2015-08-11 20:36:05 -0700

    Added sample user and content. Strip whitespaces from name to reference image

--- app/views.py
+++ app/views.py
@@ -6,7 +6,7 @@ interests = {1 : {'area':'code', 'about': 'This is the code page', 'color': '#93
 			 3 : {'area':'music', 'about': 'This is the music page', 'color': '#5487B2', 'FKUserId': 1}
 		}
 
-user = {1: {'name': 'admin','password': 'default'}}
+user = {1: {'name': 'Gentile Bellini','password': 'default'}}
 
 posts = {1: {'title': 'Python', 'text': 'I like Python', 'FK_IntrestId': 1},
 		 2: {'title': 'Flask', 'text': 'This is built on Flask', 'FK_IntrestId': 1},
