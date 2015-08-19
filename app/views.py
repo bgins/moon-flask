a962684252e5f51b960a6019e5f939c3d4c850de
Brian Ginsburg <bginsburg3@gmail.com>
2015-08-18 22:13:56 -0700

    Added posts to interest pages.

--- app/views.py
+++ app/views.py
@@ -8,11 +8,18 @@ interests = {1 : {'area':'Venice', 'about': 'My original home', 'FKUserId': 1},
 
 user = {1: {'name': 'Moon Flask','password': 'default', 'color': '#111111'}}
 
-posts = {1: {'title': 'Python', 'text': 'I like Python', 'FK_IntrestId': 1},
+posts = {1: {'title': 'The Doge', 'text': 'Aliquam ac mauris ante. Suspendisse \
+			non tempor lectus, a dignissim felis. Nunc posuere, purus et \
+			feugiat accumsan, lorem leo auctor turpis, quis vestibulum metus \
+			magna id neque. Ut ac blandit odio. Praesent at elit vestibulum, \
+			cursus erat luctus, aliquam eros. Mauris maximus dolor sed lorem \
+			rhoncus, vel fringilla mi sagittis. Suspendisse faucibus nunc eu \
+			enim pharetra, id lacinia elit venenatis. Integer interdum tellus \
+			aliquam ipsum semper consequat. ', 'FK_IntrestId': 1},
 		 2: {'title': 'Flask', 'text': 'This is built on Flask', 'FK_IntrestId': 1},
 		 3: {'title': 'Blogging', 'text': 'I write blogposts', 'FK_IntrestId': 2},
-		 1: {'title': 'Cello', 'text': 'I play the Cello', 'FK_IntrestId': 3},
-		 1: {'title': 'Audio Production', 'text': 'I produce my own music', 'FK_IntrestId': 3}
+		 4: {'title': 'Cello', 'text': 'I play the Cello', 'FK_IntrestId': 3},
+		 5: {'title': 'Audio Production', 'text': 'I produce my own music', 'FK_IntrestId': 3}
 	}
 
 @app.route('/')
@@ -21,4 +28,4 @@ def index():
 
 @app.route('/interest/<area>')
 def interest(area):
-	return render_template('interest.html', user=user, interests=interests, area=area)
+	return render_template('interest.html', user=user, interests=interests, area=area, posts=posts)
