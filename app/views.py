6a439be92449aa150acb7edd045a4f837b4b8710
Brian Ginsburg <bginsburg3@gmail.com>
2015-08-24 10:57:49 -0700

    Added conditional logic to interest page headers.

--- app/views.py
+++ app/views.py
@@ -1,12 +1,12 @@
 from app import app
 from flask import render_template, url_for
 
-interests = {1 : {'area':'Venice', 'about': 'My original home', 'FKUserId': 1}, 
-			 2 : {'area':'Constantinople', 'about': 'A peace time cultural ambassador', 'FKUserId': 1},
-			 3 : {'area':'Greece', 'about': 'Time in Greece', 'FKUserId': 1}
+interests = {1 : {'area': 'Venice', 'about': 'My original home', 'image': True, 'FKUserId': 1}, 
+			 2 : {'area': 'Constantinople', 'about': 'A peace time cultural ambassador', 'image': True, 'FKUserId': 1},
+			 3 : {'area': 'Greece', 'about': 'Time in Greece', 'image': True, 'FKUserId': 1}
 		}
 
-user = {1: {'name': 'Moon Flask','password': 'default', 'color': '#111111'}}
+user = {1: {'name': 'Moon Flask', 'password': 'default'}}
 
 # The FK_InterestIds should be changed to numbers, they match areas for now
 posts = {1: {'title': 'The Doge', 'text': 'Aliquam ac mauris ante. Suspendisse \
