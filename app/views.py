330f5dd21ea372af0a4ba72bfa7b6ce7a9c20517
Brian Ginsburg <bginsburg3@gmail.com>
2015-08-27 13:40:50 -0700

    Update page.html to receive and render 'post' db objects rather than 'posts' list.

--- app/views.py
+++ app/views.py
@@ -1,33 +1,33 @@
 from app import app, db
 from flask import render_template, url_for
 
-interests = {1 : {'area': 'Dragons', 'about': 'St. George in his famed battle \
-				  against the Dragon.', 'image': True, 'FKUserId': 1}, 
-			 2 : {'area': 'Hermits', 'about': 'Early Renaissance monastic life.',
-			  	  'image': True, 'FKUserId': 1},
-			 3 : {'area': 'Polyhedra', 'about': 'My stellated dodecahedron and more.',
-			 	  'image': True, 'FKUserId': 1}
-		}
+# interests = {1 : {'area': 'Dragons', 'about': 'St. George in his famed battle \
+# 				  against the Dragon.', 'image': True, 'FKUserId': 1}, 
+# 			 2 : {'area': 'Hermits', 'about': 'Early Renaissance monastic life.',
+# 			  	  'image': True, 'FKUserId': 1},
+# 			 3 : {'area': 'Polyhedra', 'about': 'My stellated dodecahedron and more.',
+# 			 	  'image': True, 'FKUserId': 1}
+# 		}
 
-users = {1: {'name': 'Moon Flask', 'password': 'default', 'about': 'Moon Flask is \
-			a simple content management system built on Flask. You can use it to \
-			build a portfolio or personal website. Paolo Uccello serves as the \
-			user for the sample app. Take a look at his interests below.'}}
+# users = {1: {'name': 'Moon Flask', 'password': 'default', 'about': 'Moon Flask is \
+# 			a simple content management system built on Flask. You can use it to \
+# 			build a portfolio or personal website. Paolo Uccello serves as the \
+# 			user for the sample app. Take a look at his interests below.'}}
 
 # The FK_InterestIds should be changed to numbers, they match areas for now
-posts = {1: {'title': 'Dragons in our Times.', 'text': 'Aliquam ac mauris ante. Suspendisse \
-			non tempor lectus, a dignissim felis. Nunc posuere, purus et \
-			feugiat accumsan, lorem leo auctor turpis, quis vestibulum metus \
-			magna id neque. Ut ac blandit odio. Praesent at elit vestibulum, \
-			cursus erat luctus, aliquam eros. Mauris maximus dolor sed lorem \
-			rhoncus, vel fringilla mi sagittis. Suspendisse faucibus nunc eu \
-			enim pharetra, id lacinia elit venenatis. Integer interdum tellus \
-			aliquam ipsum semper consequat. ', 'image': False, 'FK_InterestId': 'Dragons'},
-		 2: {'title': 'St. George and the Dragon', 'text': 'Brave St.George.', 'image': True, 'FK_InterestId': 'Dragons'},
-		 3: {'title': 'Episodes of the Hermit Life', 'text': 'Hermits and their role in monsatic life.', 'image': True, 'FK_InterestId': 'Hermits'},
-		 4: {'title': 'Perspective Study of Mazzocchio', 'text': 'My famous sketch.', 'image': True, 'FK_InterestId': 'Polyhedra'},
-		 5: {'title': 'More Shapes', 'text': 'Undiscovered polyhedra.', 'image': False, 'FK_InterestId': 'Polyhedra'}
-	}
+# posts = {1: {'title': 'Dragons in our Times.', 'text': 'Aliquam ac mauris ante. Suspendisse \
+# 			non tempor lectus, a dignissim felis. Nunc posuere, purus et \
+# 			feugiat accumsan, lorem leo auctor turpis, quis vestibulum metus \
+# 			magna id neque. Ut ac blandit odio. Praesent at elit vestibulum, \
+# 			cursus erat luctus, aliquam eros. Mauris maximus dolor sed lorem \
+# 			rhoncus, vel fringilla mi sagittis. Suspendisse faucibus nunc eu \
+# 			enim pharetra, id lacinia elit venenatis. Integer interdum tellus \
+# 			aliquam ipsum semper consequat. ', 'image': False, 'FK_InterestId': 'Dragons'},
+# 		 2: {'title': 'St. George and the Dragon', 'text': 'Brave St.George.', 'image': True, 'FK_InterestId': 'Dragons'},
+# 		 3: {'title': 'Episodes of the Hermit Life', 'text': 'Hermits and their role in monsatic life.', 'image': True, 'FK_InterestId': 'Hermits'},
+# 		 4: {'title': 'Perspective Study of Mazzocchio', 'text': 'My famous sketch.', 'image': True, 'FK_InterestId': 'Polyhedra'},
+# 		 5: {'title': 'More Shapes', 'text': 'Undiscovered polyhedra.', 'image': False, 'FK_InterestId': 'Polyhedra'}
+# 	}
 
 
 # Models
@@ -80,4 +80,5 @@ def index():
 @app.route('/page/<name>')
 def page(name):
 	pages = Page.query.all()
+	posts = Post.query.all()
 	return render_template('page.html', name=name, pages=pages, posts=posts)
