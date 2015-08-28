230beeeecb4812cd91659f5704235bf0ecce9125
Brian Ginsburg <bginsburg3@gmail.com>
2015-08-28 09:10:22 -0700

    Added flask-migrations and initial migration of db.

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,17 @@
 from flask import Flask
-from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.migrate import Migrate, MigrateCommand
 from flask.ext.script import Manager, Shell
+from flask.ext.sqlalchemy import SQLAlchemy
 from config import basedir
 
 app = Flask(__name__)
 app.config.from_object('config')
 # app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+
 manager = Manager(app)
 db = SQLAlchemy(app)
+
+migrate = Migrate(app, db)
+manager.add_command('db', MigrateCommand)
 
 from app import views
