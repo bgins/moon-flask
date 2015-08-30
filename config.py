8bd404477417518fe56e8dac9c8a3fd66132605b
Brian Ginsburg <bginsburg3@gmail.com>
2015-08-30 10:18:20 -0700

    Added SECRET_KEY

--- config.py
+++ config.py
@@ -4,3 +4,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+
+SECRET_KEY = os.environ.get('SECRET_KEY') or 'Crazy string that you must put into an environment variable'
