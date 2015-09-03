95f86dacfb6781e18d78bfe3f99f59ed626f7b40
Brian Ginsburg <bginsburg3@gmail.com>
2015-09-03 15:45:29 -0700

    Add requriements.txt
    Also comment out uneeded conig line.

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
-SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+# SQLALCHEMY_COMMIT_ON_TEARDOWN = True
 
 SECRET_KEY = os.environ.get('SECRET_KEY') or 'Crazy string that you must put into an environment variable'
 
